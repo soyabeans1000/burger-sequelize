08bc09e8cc3e4d747e0647da493f9e87a92589fd
Sarika <sarika@ngoyal.com>
2019-05-28 17:47:22 -0700

    ENV

--- config/index.js
+++ config/index.js
@@ -4,9 +4,12 @@ module.exports = new Sequelize('mysql://root:Reboot@2019@localhost:3306/burger_d
 
 
 
-//  if (process.env.JAWSDB_URL) {
-//    sequelize = mysql.createConnection(process.env.JAWSDB_URL)
-//  }
-//  else 
-//  {
-  
+  if (process.env.JAWSDB_URL) {
+    module.exports = new Sequelize('mysql://ifel61iqkh02i48y:xafuveyl2hjfaku6@l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l03o751ixnob7va4')
+  }
+  else 
+  {
+
+    module.exports = new Sequelize('mysql://root:Reboot@2019@localhost:3306/burger_db')
+
+  }

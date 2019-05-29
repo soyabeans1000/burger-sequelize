beaf4dd118480944857a5f24975181f5514c0a8d
Sarika <sarika@ngoyal.com>
2019-05-28 20:18:22 -0700

    Hide Connection

--- config/index.js
+++ config/index.js
@@ -2,14 +2,22 @@ const Sequelize = require('sequelize')
 
 module.exports = new Sequelize('mysql://root:Reboot@2019@localhost:3306/burger_db')
 
-
-
-  if (process.env.JAWSDB_URL) {
-    module.exports = new Sequelize('mysql://ifel61iqkh02i48y:xafuveyl2hjfaku6@l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l03o751ixnob7va4')
+//If Deployed 
+ if (process.env.JAWSDB_URL) {
+    module.exports = new Sequelize(process.env.JAWSDB_URL)
   }
   else 
   {
-
     module.exports = new Sequelize('mysql://root:Reboot@2019@localhost:3306/burger_db')
-
   }
+
+
+  
+// //If Deployed 
+//  if (process.env.JAWSDB_URL) {
+//   module.exports = new Sequelize('mysql://ifel61iqkh02i48y:xafuveyl2hjfaku6@l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l03o751ixnob7va4')
+// }
+// else 
+// {
+//   module.exports = new Sequelize('mysql://root:Reboot@2019@localhost:3306/burger_db')
+// }
